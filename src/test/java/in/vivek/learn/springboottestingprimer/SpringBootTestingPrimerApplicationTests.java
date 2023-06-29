b0492a9a2ac00857ab2c18910be0de5d1d7c09d6
Vivek Varma <vsquared101@gmail.com>
2023-06-29 22:39:15 +0530

    First spring boot testing primer commit

---
+++ src/test/java/in/vivek/learn/springboottestingprimer/SpringBootTestingPrimerApplicationTests.java
@@ -0,0 +1,13 @@
+package in.vivek.learn.springboottestingprimer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingPrimerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
